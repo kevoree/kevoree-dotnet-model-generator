2e772c096c72cdc7fe3808154cfdbe30b52393ea
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-14 14:43:47 +0200

    Migrated from f# to c# because of compatibiliy issues

---
+++ EmptyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace kevoreedotnetmodelgenerator2
+{
+
+	[TestFixture]
+	public class EmptyClass
+	{
+		public EmptyClass ()
+		{
+		}
+
+		[Test]
+		public void Test()
+		{
+			new MyClass ().loadDll (@"/home/mleduc/dev/dotnet/kevoree-dotnet-comp-yield-version/bin/Release/YieldVersion.dll");
+		}
+	}
+}
+
